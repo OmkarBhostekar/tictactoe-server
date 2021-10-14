81c6108c2e2054b3ea15835ed9b8d4306e41b394
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2021-10-15 02:54:08 +0530

    player icons added X and O

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const uuid = require('uuid')
 const app = require('./app');
-const { randomUUID } = require('crypto');
 const http = require('http').Server(app)
 var io = require('socket.io')(http);
 const port = process.env.PORT || 3000
@@ -20,12 +19,11 @@ const joinRoom = (socket, room) => {
 }
 
 io.on('connection', socket => {
-    console.log('connected');
 
     socket.on('move', (data) => {
         const room = rooms[data.roomId]
-        if (socket.id === room.sockets[room.nextMove]) {
-            room.moves[data.boxId] = socket.id
+        if (socket.id === room.sockets[room.nextMove] && room.moves[data.boxId] === '') {
+            room.moves[data.boxId] = room.nextMove === 0 ? room.player1Icon : room.player2Icon
 
             io.sockets.in(room.id).emit('move', {
                 boxId: data.boxId,
@@ -37,7 +35,7 @@ io.on('connection', socket => {
             } else {
                 room.nextMove = 0
             }
-            nextMove(socket, room)
+            checkResult(socket, room)
             console.log(room.moves);
         }
     })
@@ -50,7 +48,9 @@ io.on('connection', socket => {
             ready1: false,
             ready2: false,
             moves: ['', '', '', '', '', '', '', '', ''],
-            nextMove: 0
+            nextMove: 0,
+            player1Icon: '',
+            player2Icon: '',
         }
         rooms[room.id] = room
         joinRoom(socket, room)
@@ -69,21 +69,35 @@ io.on('connection', socket => {
 
     socket.on('ready', (data) => {
         const room = rooms[data.roomId]
-        if (room.ready1) {
-            // start game
-            startGame(socket, room)
-        } else {
-            room.ready1 = true
-            io.sockets.in(room.id).emit('status', 'player 1 is ready')
+        if (room.sockets.length === 2) {
+            if (room.ready1) {
+                // start game
+                startGame(socket, room)
+            } else {
+                room.ready1 = true
+                io.sockets.in(room.id).emit('status', 'player 1 is ready')
+            }
         }
     })
+
+    socket.on('choose', (data) => {
+        const room = rooms[data.roomId]
+        room.player1Icon = room.nextMove === 0 ? data.icon1 : data.icon2
+        room.player2Icon = room.nextMove === 0 ? data.icon2 : data.icon1
+        nextMove(socket, room)
+    })
 })
+
+const checkResult = (socket, room) => {
+    // TODO implement result check logic
+    nextMove(socket, room)
+}
 
 const startGame = (socket, room) => {
     io.sockets.in(room.id).emit('status', 'start game')
     const rnd = Math.floor(Math.random() * 2)
-    room.currentMove = rnd
-    nextMove(socket, room)
+    room.nextMove = rnd
+    chooseIcon(room.sockets[rnd], room)
     console.log(room)
     console.log('start game')
 }
@@ -93,6 +107,12 @@ const nextMove = (socket, room) => {
         playerId: room.sockets[room.nextMove]
     })
     console.log(room);
+}
+
+const chooseIcon = (id, room) => {
+    io.sockets.to(id).emit('choose', {
+        playerId: room.sockets[room.nextMove]
+    })
 }
 
 const server = http.listen(port, () => {
