bbc4867af4ff0281738b8d56b6dcfb0199b1a69b
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-07 22:55:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const uuid = require('uuid')
 const app = require('./app');
 const http = require('http').Server(app)
 var io = require('socket.io')(http)
+io.set('transports', ['websocket'])
 const port = process.env.PORT || 3000
 
 dotenv.config({ path: './config/config.env' })
